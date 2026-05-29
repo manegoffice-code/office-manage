21d02551d77afadff0933b18857f3ad05c71c741
manegoffice-code <manegoffice@gmail.com>
2026-05-29 11:50:20 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: `${BASE_URL}`,
+  baseURL: `${BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
